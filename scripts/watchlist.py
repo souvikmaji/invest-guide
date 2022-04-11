f9b7d572476d663f5e33756f3c004d944a3045e8
souvik-rzp <souvik.maji@razorpay.com>
2022-04-11 09:54:28 +0530

    using tomorrow as end date

--- scripts/watchlist.py
+++ scripts/watchlist.py
@@ -3,13 +3,12 @@ import pandas_ta as ta
 import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
-from datetime import date
+from datetime import date, timedelta
 from email_sending import Notifier
-
 
 ghcl = yf.Ticker('GHCL.NS')
 start_date = date(2020,8,31)
-end_date = date.today()
+end_date = date.today() + timedelta(days=1)
 
 ghcl_df = ghcl.history(start=start_date, end=end_date)
 ghcl_df.ta.supertrend(period=10, multiplier=3, append = True)
