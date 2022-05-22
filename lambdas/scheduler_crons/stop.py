241df64b7e51bf6ac86d91042b1c66234050c768
souvik-rzp <souvik.maji@razorpay.com>
2022-05-22 11:18:02 +0530

    activated xray tracing

--- lambdas/scheduler_crons/stop.py
+++ lambdas/scheduler_crons/stop.py
@@ -2,12 +2,15 @@ import boto3
 import logging
 from util import getInstanceIds, isHoliday, createSuccessResponse
 
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
 
 region = 'ap-south-1'
 ec2 = boto3.client('ec2', region_name=region)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+patch_all()
 
 def handler(event, context):
     if isHoliday():
